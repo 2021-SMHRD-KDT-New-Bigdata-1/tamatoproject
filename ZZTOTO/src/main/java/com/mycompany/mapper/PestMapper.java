8d5994d3136bf34a7d463a50e7a6a22bf0cfbe93
smhrd <smhrd@DESKTOP-PSP70JT>
2021-12-02 21:43:57 +0900

    영농일지 기능 구현

--- ZZTOTO/src/main/java/com/mycompany/mapper/PestMapper.java
+++ ZZTOTO/src/main/java/com/mycompany/mapper/PestMapper.java
@@ -35,10 +35,10 @@ public interface PestMapper {
 	@Insert("insert into farm_diaries (diary_subject, diary_content, issue, reg_date, member_id) VALUES (#{diary_subject}, #{diary_content}, #{issue}, NOW(), #{member_id})")
 	public void diaryInsertAjax(Farm_diary vo);
   	
-  	@Update("update farm_diaries set diary_subject=#{diary_subject}, where diary_num=#{diary_num}")
+  	@Update("update farm_diaries set diary_subject=#{diary_subject} where diary_num=#{diary_num}")
 	public void diaryUpdateAjax(Farm_diary vo); // XML(X) -> update SQL~
   	
-  	@Update("update farm_diaries set diary_subject=#{diary_subject}, where diary_num=#{diary_num}")
+  	@Update("update farm_diaries set diary_content=#{diary_content}, issue=#{issue} where diary_num=#{diary_num}")
 	public void diaryUpdateAjax1(Farm_diary vo); // XML(X) -> update SQL~
 
 }
